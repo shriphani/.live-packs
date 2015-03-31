4fe0d234c47a2efccb4ecc242e71ad1ef852119b
Shriphani Palakodety <shriphanip@gmail.com>
2015-03-31 15:00:11 -0700

    cuda mode now also loads c++

--- shriphani-pack/init.el
+++ shriphani-pack/init.el
@@ -16,3 +16,28 @@
 
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
+
+(defun my-c++-mode-hook ()
+  (setq c-basic-offset 4)
+  (c-set-offset 'substatement-open 0))
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
+
+(add-hook 'cuda-mode-hook 'my-c++-mode-hook)
+
+(add-to-list 'auto-mode-alist '("\\.cu$" . c++-mode))
+
+(add-to-list 'auto-mode-alist '("\\.cu$" . cuda-mode))
+
+(defun get-default-height ()
+       (/ (- (display-pixel-height) 120)
+          (frame-char-height)))
+
+(add-to-list 'default-frame-alist '(width . 140))
+(add-to-list 'default-frame-alist (cons 'height (get-default-height)))
+
+(add-hook 'cuda-mode-hook
+          'c++-mode)
+
+(require 'mmm-auto)
+(setq mmm-global-mode 'maybe)
+(mmm-add-mode-ext-class 'cuda-mode "\\.cu\\'" 'c++-mode)
